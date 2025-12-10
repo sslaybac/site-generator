fbddad8276e0bca5529bc6fe77d3ac2cb71a416a
Mike Slayback <Scott.M.Slayback@gmail.com>
2025-12-10 14:02:12 -0800

    COmpleted Chapter 3: Nodes

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,7 @@
 import unittest
 
-from textnode import TextNode, TextType, text_node_to_html_node
+from textnode import TextNode, TextType, split_nodes_image, split_nodes_link, text_node_to_html_node, convert_line_to_textnodes
+from textnode import extract_markdown_images, extract_markdown_links
 
 
 class TestTextNode(unittest.TestCase):
@@ -57,6 +58,201 @@ class TestTextNode(unittest.TestCase):
 		self.assertEqual(html_node.value, "")
 		self.assertEqual(html_node.props["alt"], "This is a image node")
 		self.assertEqual(html_node.props["src"], "www.image.com")
+
+	def test_line_to_bold_simple(self):
+		md_line = "this is a **bold** test message"
+		nodes = convert_line_to_textnodes(md_line)
+		self.assertEqual(len(nodes), 3)
+		self.assertEqual(nodes[0].text_type, TextType.PLAIN)
+		self.assertEqual(nodes[1].text_type, TextType.BOLD)
+		self.assertEqual(nodes[1].text, "bold")
+		self.assertEqual(nodes[2].text_type, TextType.PLAIN)
+
+		md_line2 = "**bold** at the beginning"
+		nodes = convert_line_to_textnodes(md_line)
+		self.assertEqual(len(nodes), 3)
+		self.assertEqual(nodes[0].text_type, TextType.PLAIN)
+		self.assertEqual(nodes[1].text_type, TextType.BOLD)
+		self.assertEqual(nodes[1].text, "bold")
+		self.assertEqual(nodes[2].text_type, TextType.PLAIN)
+
+		md_line3 = "line ends in **bold**"
+		nodes = convert_line_to_textnodes(md_line)
+		self.assertEqual(len(nodes), 3)
+		self.assertEqual(nodes[0].text_type, TextType.PLAIN)
+		self.assertEqual(nodes[1].text_type, TextType.BOLD)
+		self.assertEqual(nodes[1].text, "bold")
+		self.assertEqual(nodes[2].text_type, TextType.PLAIN)
+
+	def test_line_to_italic_simple(self):
+		md_line = "this is a _italic_ test message"
+		nodes = convert_line_to_textnodes(md_line)
+		self.assertEqual(len(nodes), 3)
+		self.assertEqual(nodes[0].text_type, TextType.PLAIN)
+		self.assertEqual(nodes[1].text_type, TextType.ITALIC)
+		self.assertEqual(nodes[1].text, "italic")
+		self.assertEqual(nodes[2].text_type, TextType.PLAIN)
+
+		md_line2 = "_italic_ at the beginning"
+		nodes = convert_line_to_textnodes(md_line)
+		self.assertEqual(len(nodes), 3)
+		self.assertEqual(nodes[0].text_type, TextType.PLAIN)
+		self.assertEqual(nodes[1].text_type, TextType.ITALIC)
+		self.assertEqual(nodes[1].text, "italic")
+		self.assertEqual(nodes[2].text_type, TextType.PLAIN)
+
+		md_line3 = "line ends in _italic_"
+		nodes = convert_line_to_textnodes(md_line)
+		self.assertEqual(len(nodes), 3)
+		self.assertEqual(nodes[0].text_type, TextType.PLAIN)
+		self.assertEqual(nodes[1].text_type, TextType.ITALIC)
+		self.assertEqual(nodes[1].text, "italic")
+		self.assertEqual(nodes[2].text_type, TextType.PLAIN)
+
+	def test_line_to_code_simple(self):
+		md_line = "this is a `code` test message"
+		nodes = convert_line_to_textnodes(md_line)
+		self.assertEqual(len(nodes), 3)
+		self.assertEqual(nodes[0].text_type, TextType.PLAIN)
+		self.assertEqual(nodes[1].text_type, TextType.CODE)
+		self.assertEqual(nodes[1].text, "code")
+		self.assertEqual(nodes[2].text_type, TextType.PLAIN)
+
+		md_line2 = "`code` at the beginning"
+		nodes = convert_line_to_textnodes(md_line)
+		self.assertEqual(len(nodes), 3)
+		self.assertEqual(nodes[0].text_type, TextType.PLAIN)
+		self.assertEqual(nodes[1].text_type, TextType.CODE)
+		self.assertEqual(nodes[1].text, "code")
+		self.assertEqual(nodes[2].text_type, TextType.PLAIN)
+
+		md_line3 = "line ends in `code`"
+		nodes = convert_line_to_textnodes(md_line)
+		self.assertEqual(len(nodes), 3)
+		self.assertEqual(nodes[0].text_type, TextType.PLAIN)
+		self.assertEqual(nodes[1].text_type, TextType.CODE)
+		self.assertEqual(nodes[1].text, "code")
+		self.assertEqual(nodes[2].text_type, TextType.PLAIN)
+
+	def test_double_bold(self):
+		md_line = "testing **bold** two **bold** sections"
+		nodes = convert_line_to_textnodes(md_line)
+		self.assertEqual(len(nodes), 5)
+		self.assertEqual(nodes[0].text_type, TextType.PLAIN)
+		self.assertEqual(nodes[1].text_type, TextType.BOLD)
+		self.assertEqual(nodes[1].text, "bold")
+		self.assertEqual(nodes[2].text_type, TextType.PLAIN)
+		self.assertEqual(nodes[3].text_type, TextType.BOLD)
+		self.assertEqual(nodes[3].text, "bold")
+		self.assertEqual(nodes[4].text_type, TextType.PLAIN)
+
+	def test_double_italic(self):
+		md_line = "testing _italic_ two _italic_ sections"
+		nodes = convert_line_to_textnodes(md_line)
+		self.assertEqual(len(nodes), 5)
+		self.assertEqual(nodes[0].text_type, TextType.PLAIN)
+		self.assertEqual(nodes[1].text_type, TextType.ITALIC)
+		self.assertEqual(nodes[1].text, "italic")
+		self.assertEqual(nodes[2].text_type, TextType.PLAIN)
+		self.assertEqual(nodes[3].text_type, TextType.ITALIC)
+		self.assertEqual(nodes[3].text, "italic")
+		self.assertEqual(nodes[4].text_type, TextType.PLAIN)
+
+	def test_double_code(self):
+		md_line = "testing `code` two `code` sections"
+		nodes = convert_line_to_textnodes(md_line)
+		self.assertEqual(len(nodes), 5)
+		self.assertEqual(nodes[0].text_type, TextType.PLAIN)
+		self.assertEqual(nodes[1].text_type, TextType.CODE)
+		self.assertEqual(nodes[1].text, "code")
+		self.assertEqual(nodes[2].text_type, TextType.PLAIN)
+		self.assertEqual(nodes[3].text_type, TextType.CODE)
+		self.assertEqual(nodes[3].text, "code")
+		self.assertEqual(nodes[4].text_type, TextType.PLAIN)
+
+	def test_mixed_line(self):
+		md_line = "testing **bold** then _italic_ then `code` line"
+		nodes = convert_line_to_textnodes(md_line)
+		self.assertEqual(len(nodes), 7)
+		self.assertEqual(nodes[0].text_type, TextType.PLAIN)
+		self.assertEqual(nodes[1].text_type, TextType.BOLD)
+		self.assertEqual(nodes[1].text, "bold")
+		self.assertEqual(nodes[2].text_type, TextType.PLAIN)
+		self.assertEqual(nodes[3].text_type, TextType.ITALIC)
+		self.assertEqual(nodes[3].text, "italic")
+		self.assertEqual(nodes[4].text_type, TextType.PLAIN)
+		self.assertEqual(nodes[5].text_type, TextType.CODE)
+		self.assertEqual(nodes[5].text, "code")
+		self.assertEqual(nodes[6].text_type, TextType.PLAIN)
+
+	def test_image_extractor(self):
+		text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+		expected = [('rick roll', 'https://i.imgur.com/aKaOqIh.gif'), ('obi wan', 'https://i.imgur.com/fJRm4Vk.jpeg')]
+		self.assertEqual(extract_markdown_images(text), expected)
+
+		text = "no images here"
+		expected = []
+		self.assertEqual(extract_markdown_images(text), expected)
+		
+
+	def test_link_extractor(self):
+		text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+		expected = [('to boot dev', 'https://www.boot.dev'), ('to youtube', 'https://www.youtube.com/@bootdotdev')]
+		self.assertEqual(extract_markdown_links(text), expected)
+
+		text = "no links here"
+		expected = []
+		self.assertEqual(extract_markdown_images(text), expected)
+
+	def test_split_images(self):
+		node = TextNode(
+			"This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+			TextType.PLAIN,
+		)
+		new_nodes = split_nodes_image([node])
+		self.assertListEqual(
+			[
+				TextNode("This is text with an ", TextType.PLAIN),
+				TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+				TextNode(" and another ", TextType.PLAIN),
+				TextNode("second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"),
+			],
+			new_nodes,
+		)
+
+	def test_split_links(self):
+		node = TextNode(
+			"This is text with a [link](https://i.imgur.com/zjjcJKZ.png) and another [second link](https://i.imgur.com/3elNhQu.png)",
+			TextType.PLAIN,
+		)
+		new_nodes = split_nodes_link([node])
+		self.assertListEqual(
+			[
+				TextNode("This is text with a ", TextType.PLAIN),
+				TextNode("link", TextType.LINK, "https://i.imgur.com/zjjcJKZ.png"),
+				TextNode(" and another ", TextType.PLAIN),
+				TextNode("second link", TextType.LINK, "https://i.imgur.com/3elNhQu.png"),
+			],
+			new_nodes,
+		)
+
+	def test_split_all(self):
+		input = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+		actual = convert_line_to_textnodes(input)
+		expected = [
+			TextNode("This is ", TextType.PLAIN),
+			TextNode("text", TextType.BOLD),
+			TextNode(" with an ", TextType.PLAIN),
+			TextNode("italic", TextType.ITALIC),
+			TextNode(" word and a ", TextType.PLAIN),
+			TextNode("code block", TextType.CODE),
+			TextNode(" and an ", TextType.PLAIN),
+			TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+			TextNode(" and a ", TextType.PLAIN),
+			TextNode("link", TextType.LINK, "https://boot.dev"),
+		]
+		self.assertListEqual(expected, actual)
+
 
 if __name__ == "__main__":
 	unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from leafnode import LeafNode
+import re
 
 class TextType(Enum):
 	PLAIN = "plain"
@@ -26,7 +27,7 @@ class TextNode:
 			return True
 
 	def __repr__(self):
-		return f"TextNode({self.text}, {self.text_type}, {self.url})"
+		return f"TextNode('{self.text}', {self.text_type}, {self.url})"
 
 def text_node_to_html_node(text_node):
 	match text_node.text_type:
@@ -44,3 +45,112 @@ def text_node_to_html_node(text_node):
 			return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
 		case _:
 			raise Exception("Invalid TextNode type.")
+
+"""
+Accepts a list of premade TextNode objects, and breaks
+the 'plaintext' nodes into subnodes, based on the first
+appearance of the delimiter
+
+This is a recursive function. Each apparent PLAIN node will
+be assessed based on the same delimiter, until no further
+sections can be broken out. A calling function will be responsible
+for re-executing this function for each type of delimiter
+"""
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+	new_nodes = []
+	for node in old_nodes:
+		if node.text_type == TextType.PLAIN:
+			substrings = node.text.split(delimiter, maxsplit=2)
+			if len(substrings) == 1: # No delimiters, no splits
+				new_nodes.append(TextNode(node.text, TextType.PLAIN))
+			elif len(substrings) == 2:
+				raise Exception(f"invalid syntax: {text_type} nodes require 2 '{delimiter}' delimiters")
+			else: # Found two matching delimeters
+				first_node = TextNode(substrings[0], TextType.PLAIN)
+				new_nodes.extend(split_nodes_delimiter([first_node], delimiter, text_type))
+
+				# We will ignore nesting at this stage
+				new_nodes.append(TextNode(substrings[1], text_type))
+
+				third_node = TextNode(substrings[2], TextType.PLAIN)
+				new_nodes.extend(split_nodes_delimiter([third_node], delimiter, text_type))
+		else: # ignore nesting
+			new_nodes.append(node)
+	return new_nodes
+
+"""
+function for converting a line of markdown text to a list of type-specific TextNodes.
+Currently, the sequence is:
+1. Bold: '**'
+2. Italic: '_'
+3. Code: '`'
+"""
+def convert_line_to_textnodes(text):
+	starting_point = [TextNode(text, TextType.PLAIN)]
+	bold_stage = split_nodes_delimiter(starting_point, "**", TextType.BOLD)
+	italic_stage = split_nodes_delimiter(bold_stage, "_", TextType.ITALIC)
+	code_stage = split_nodes_delimiter(italic_stage, "`", TextType.CODE)
+	image_stage = split_nodes_image(code_stage)
+	link_stage = split_nodes_link(image_stage)
+	return link_stage
+
+"""
+function for identifying images in markdown.
+Uses regular expressions to extract link urls and return tuples
+parameter:
+text: a line of markdown text
+"""
+def extract_markdown_images(text):
+	image_expr = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+	return re.findall(image_expr, text)
+	
+"""
+function for identifying images in markdown.
+Uses regular expressions to extract link urls and return tuples
+parameter:
+text: a line of markdown text
+"""
+def extract_markdown_links(text):
+	link_expr = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+	return re.findall(link_expr, text)
+
+"""
+function to (possibly) split a plaintext node into a list of
+plaintext nodes and image nodes.
+"""
+def split_nodes_image(old_nodes):
+	new_nodes = []
+	for node in old_nodes:
+		if node.text_type != TextType.PLAIN:
+			new_nodes.append(node)
+			continue
+		images = extract_markdown_images(node.text)
+		remainder = node.text
+		for image in images:
+			delim = f"![{image[0]}]({image[1]})"
+			substrings = remainder.split(delim, maxsplit=1)
+			new_nodes.append(TextNode(substrings[0], TextType.PLAIN))
+			new_nodes.append(TextNode(image[0], TextType.IMAGE, image[1]))
+			remainder = substrings[1]
+		if remainder:
+			new_nodes.append(TextNode(remainder, TextType.PLAIN))
+	return new_nodes
+
+
+def split_nodes_link(old_nodes):
+	new_nodes = []
+	for node in old_nodes:
+		if node.text_type != TextType.PLAIN:
+			new_nodes.append(node)
+			continue
+		links = extract_markdown_links(node.text)
+		remainder = node.text
+		for link in links:
+			delim = f"[{link[0]}]({link[1]})"
+			substrings = remainder.split(delim, maxsplit=1)
+			new_nodes.append(TextNode(substrings[0], TextType.PLAIN))
+			new_nodes.append(TextNode(link[0], TextType.LINK, link[1]))
+			remainder = substrings[1]
+		if remainder:
+			new_nodes.append(TextNode(remainder, TextType.PLAIN))
+	return new_nodes
