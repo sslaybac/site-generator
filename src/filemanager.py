d1de8c0ab7912ff3ea97a0922517628ca0346979
Mike Slayback <Scott.M.Slayback@gmail.com>
2025-12-23 13:26:46 -0800

    corrected static folder structure. Also corrected path construction errors.

--- src/filemanager.py
+++ src/filemanager.py
@@ -22,10 +22,14 @@ def copy_dir(dirpath):
 	dst_dir = os.path.join(DST_DIR, dirpath)
 	files = os.listdir(src_dir)
 	for f in files:
-		if os.path.isdir(f):
-			new_path = os.path.join(dirpath, f)
+		new_path = os.path.join(dirpath, f)
+		print(f"new_path = {new_path}")
+		if os.path.isdir(os.path.join(SRC_DIR, new_path)):
+			os.mkdir(os.path.join(DST_DIR, new_path))
 			copy_dir(new_path)
 		else:
 			src = os.path.join(src_dir, f)
+			print(f"source: {src}")
 			dst = os.path.join(dst_dir, f)
+			print(f"destination: {dst}")
 			shutil.copy(src, dst)
--- src/main.py
+++ src/main.py
@@ -5,6 +5,5 @@ from filemanager import copy_static_to_public
 def main():
 	copy_static_to_public()
 
-
 if __name__ == "__main__":
 	main()
